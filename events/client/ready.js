382301b2bbe8c2aa06378153d1ca543c058d2c3b
PhantHive <zakaria.chaouki@ipsa.fr>
2022-04-04 23:11:54 +0200

    update ready.js add slash commands functions

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,9 @@
-const { globalCmd, studentCmd } = require('../../dataHandler.js');
+const { globalCmd, studentCmd, trainCmd } = require('../../dataHandler.js');
 const {trainCmd} = require("../../dataHandler");
 
 module.exports = (client) => {
     console.log("Z-MUSE ONLINE")
     globalCmd(client);
-    trainCmd(client, "502931781012684818");
     studentCmd(client, "880499115878932571");
+    trainCmd(client, "502931781012684818");
 }
